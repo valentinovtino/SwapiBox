34e7e80a8288ba1b02c16483d1cff0b5fa728a36
Valentino Valentin <valentinovalentine95@gmail.com>
2018-05-13 18:44:26 -0600

    add more appTest

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -2,42 +2,42 @@ import { fetchApi, singleUrl } from './api.js';
 
 describe('api', () => {
 
-    beforeEach(() => {
-        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-            status: 200,
-            json: () => Promise.resolve({
-                results: 'return results',
-            })
-        }))        
-    })
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({
+        results: 'return results',
+      })
+    }));        
+  });
 
-    it('should call fetchApi', async ()=> {
-        expect(window.fetch).not.toHaveBeenCalled()
-        await fetchApi()
-        expect(window.fetch).toHaveBeenCalled()
-    })
+  it('should call fetchApi', async ()=> {
+    expect(window.fetch).not.toHaveBeenCalled();
+    await fetchApi();
+    expect(window.fetch).toHaveBeenCalled();
+  });
 
-    it('should throw an error in fetchApi when error is hit', () => {
-        window.fetch = jest.fn().mockImplementation(() => Promise.reject({
-            status: 404,
-        }))
+  it('should throw an error in fetchApi when error is hit', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.reject({
+      status: 404,
+    }));
 
-        const expectedError = Error('Initial fetch failed')
-        expect(fetchApi('')).rejects.toEqual(expectedError)
-    })
+    const expectedError = Error('Initial fetch failed');
+    expect(fetchApi('')).rejects.toEqual(expectedError);
+  });
 
-    it('should call singleUrl', async ()=> {
-        expect(window.fetch).not.toHaveBeenCalled()
-        await singleUrl()
-        expect(window.fetch).toHaveBeenCalled()
-    })
+  it('should call singleUrl', async ()=> {
+    expect(window.fetch).not.toHaveBeenCalled();
+    await singleUrl();
+    expect(window.fetch).toHaveBeenCalled();
+  });
 
-    it('should throw an error in singleUrl when error is hit', () => {
-        window.fetch = jest.fn().mockImplementation(() => Promise.reject({
-            status: 404,
-        }))
+  it('should throw an error in singleUrl when error is hit', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.reject({
+      status: 404,
+    }));
 
-        const expectedError = Error('Single Url fetch failed')
-        expect(singleUrl('')).rejects.toEqual(expectedError)
-    })
-})
+    const expectedError = Error('Single Url fetch failed');
+    expect(singleUrl('')).rejects.toEqual(expectedError);
+  });
+});
--- src/mockData.js
+++ src/mockData.js
@@ -1,120 +1,120 @@
 export const mockPeople = [ 
-    {
-        CharacterName: "Luke Skywalker",
-        HomeWorld: "Tatooine",
-        Population: "200000",
-        species: "Human"
-    }, 
-    {
-        CharacterName: "C-3PO",
-        HomeWorld: "Tatooine",
-        Population: "200000",
-        species: "Droid"
-    }
-]
+  {
+    CharacterName: "Luke Skywalker",
+    HomeWorld: "Tatooine",
+    Population: "200000",
+    species: "Human"
+  }, 
+  {
+    CharacterName: "C-3PO",
+    HomeWorld: "Tatooine",
+    Population: "200000",
+    species: "Droid"
+  }
+];
 
 export const mockPeopleObj = {
-    results: [
-        { 
-            "name": "Luke Skywalker", 
-            "height": "172", 
-            "mass": "77", 
-            "hair_color": "blond", 
-            "skin_color": "fair", 
-            "eye_color": "blue", 
-            "birth_year": "19BBY", 
-            "gender": "male", 
-            "homeworld": "https://swapi.co/api/planets/1/", 
-            "films": [
-                "https://swapi.co/api/films/2/", 
-                "https://swapi.co/api/films/6/", 
-                "https://swapi.co/api/films/3/", 
-                "https://swapi.co/api/films/1/", 
-                "https://swapi.co/api/films/7/"
-            ], 
-            "species": [
-                "https://swapi.co/api/species/1/"
-            ]
-         }
-    ]
-}
-
-export const mockHomeWorld = {
-    "name": "Tatooine", 
-    "rotation_period": "23", 
-    "orbital_period": "304", 
-    "diameter": "10465", 
-    "climate": "arid", 
-    "gravity": "1 standard", 
-    "terrain": "desert", 
-    "surface_water": "1", 
-    "population": "200000", 
-    "residents": [
-        "https://swapi.co/api/people/1/", 
-        "https://swapi.co/api/people/2/", 
-        "https://swapi.co/api/people/4/", 
-        "https://swapi.co/api/people/6/", 
-        "https://swapi.co/api/people/7/", 
-        "https://swapi.co/api/people/8/", 
-        "https://swapi.co/api/people/9/", 
-        "https://swapi.co/api/people/11/", 
-        "https://swapi.co/api/people/43/", 
-        "https://swapi.co/api/people/62/"
-    ], 
-    "films": [
-        "https://swapi.co/api/films/5/", 
-        "https://swapi.co/api/films/4/", 
+  results: [
+    { 
+      "name": "Luke Skywalker", 
+      "height": "172", 
+      "mass": "77", 
+      "hair_color": "blond", 
+      "skin_color": "fair", 
+      "eye_color": "blue", 
+      "birth_year": "19BBY", 
+      "gender": "male", 
+      "homeworld": "https://swapi.co/api/planets/1/", 
+      "films": [
+        "https://swapi.co/api/films/2/", 
         "https://swapi.co/api/films/6/", 
         "https://swapi.co/api/films/3/", 
-        "https://swapi.co/api/films/1/"
-    ], 
-    "created": "2014-12-09T13:50:49.641000Z", 
-    "edited": "2014-12-21T20:48:04.175778Z", 
-    "url": "https://swapi.co/api/planets/1/"
-}
+        "https://swapi.co/api/films/1/", 
+        "https://swapi.co/api/films/7/"
+      ], 
+      "species": [
+        "https://swapi.co/api/species/1/"
+      ]
+    }
+  ]
+};
+
+export const mockHomeWorld = {
+  "name": "Tatooine", 
+  "rotation_period": "23", 
+  "orbital_period": "304", 
+  "diameter": "10465", 
+  "climate": "arid", 
+  "gravity": "1 standard", 
+  "terrain": "desert", 
+  "surface_water": "1", 
+  "population": "200000", 
+  "residents": [
+    "https://swapi.co/api/people/1/", 
+    "https://swapi.co/api/people/2/", 
+    "https://swapi.co/api/people/4/", 
+    "https://swapi.co/api/people/6/", 
+    "https://swapi.co/api/people/7/", 
+    "https://swapi.co/api/people/8/", 
+    "https://swapi.co/api/people/9/", 
+    "https://swapi.co/api/people/11/", 
+    "https://swapi.co/api/people/43/", 
+    "https://swapi.co/api/people/62/"
+  ], 
+  "films": [
+    "https://swapi.co/api/films/5/", 
+    "https://swapi.co/api/films/4/", 
+    "https://swapi.co/api/films/6/", 
+    "https://swapi.co/api/films/3/", 
+    "https://swapi.co/api/films/1/"
+  ], 
+  "created": "2014-12-09T13:50:49.641000Z", 
+  "edited": "2014-12-21T20:48:04.175778Z", 
+  "url": "https://swapi.co/api/planets/1/"
+};
 
 export const mockPlanetObj = {
-    results: [
-        {
-            "name": "Alderaan", 
-            "rotation_period": "24", 
-            "orbital_period": "364", 
-            "diameter": "12500", 
-            "climate": "temperate", 
-            "gravity": "1 standard", 
-            "terrain": "grasslands, mountains", 
-            "surface_water": "40", 
-            "population": "2000000000", 
-            "residents": [
-                "https://swapi.co/api/people/5/", 
-                "https://swapi.co/api/people/68/", 
-                "https://swapi.co/api/people/81/",
+  results: [
+    {
+      "name": "Alderaan", 
+      "rotation_period": "24", 
+      "orbital_period": "364", 
+      "diameter": "12500", 
+      "climate": "temperate", 
+      "gravity": "1 standard", 
+      "terrain": "grasslands, mountains", 
+      "surface_water": "40", 
+      "population": "2000000000", 
+      "residents": [
+        "https://swapi.co/api/people/5/", 
+        "https://swapi.co/api/people/68/", 
+        "https://swapi.co/api/people/81/",
                 
-            ], 
-            "films": [
-                "https://swapi.co/api/films/6/", 
-                "https://swapi.co/api/films/1/"
-            ], 
-            "created": "2014-12-10T11:35:48.479000Z", 
-            "edited": "2014-12-20T20:58:18.420000Z", 
-            "url": "https://swapi.co/api/planets/2/"
-        }
-    ]
-}
+      ], 
+      "films": [
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/1/"
+      ], 
+      "created": "2014-12-10T11:35:48.479000Z", 
+      "edited": "2014-12-20T20:58:18.420000Z", 
+      "url": "https://swapi.co/api/planets/2/"
+    }
+  ]
+};
 
 export const mockPlanets = [
-    {
-      name:'earth',
-      terrain: 'rocky',
-      climate: 'moderate',
-      population: 12,
-      residents: [`https://swapi.co/api/people/5`]
-    },
-    {
-      name: 'mars',
-      terrain: 'sandy',
-      climate: 'hot',
-      population: 29333,
-      residents: [`https://swapi.co/api/people/4`]
-    }
-  ];
+  {
+    name:'earth',
+    terrain: 'rocky',
+    climate: 'moderate',
+    population: 12,
+    residents: [`https://swapi.co/api/people/5`]
+  },
+  {
+    name: 'mars',
+    terrain: 'sandy',
+    climate: 'hot',
+    population: 29333,
+    residents: [`https://swapi.co/api/people/4`]
+  }
+];
